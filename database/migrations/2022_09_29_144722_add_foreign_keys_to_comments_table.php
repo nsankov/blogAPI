8d017528616d7fd9f8bf104ea4e55af96bf9cce1
Nick Sankov <hikitoz@gmail.com>
2022-09-29 19:48:20 +0300

    add foreign key to comments.parent_id

--- database/migrations/2022_09_29_144722_add_foreign_keys_to_comments_table.php
+++ database/migrations/2022_09_29_144722_add_foreign_keys_to_comments_table.php
@@ -14,6 +14,7 @@ class AddForeignKeysToCommentsTable extends Migration
     public function up()
     {
         Schema::table('comments', function (Blueprint $table) {
+            $table->foreign(['parent_id'], 'comments_comments_id_fk')->references(['id'])->on('comments')->onUpdate('NO ACTION')->onDelete('NO ACTION');
             $table->foreign(['post_id'], 'comments_posts_id_fk')->references(['id'])->on('posts')->onUpdate('NO ACTION')->onDelete('NO ACTION');
         });
     }
@@ -26,6 +27,7 @@ class AddForeignKeysToCommentsTable extends Migration
     public function down()
     {
         Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_comments_id_fk');
             $table->dropForeign('comments_posts_id_fk');
         });
     }
